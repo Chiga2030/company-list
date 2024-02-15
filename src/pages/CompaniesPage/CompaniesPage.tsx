0c794214429742178044c8ba8c244c029a4ee43b
Anton Tkachuk <tkachuk.ant@mail.ru>
2024-02-15 21:41:29 +0300

    created main app page

--- src/pages/CompaniesPage/CompaniesPage.tsx
+++ src/pages/CompaniesPage/CompaniesPage.tsx
@@ -1,10 +1,13 @@
 import { CompanyListWidget, } from '../../widgets/CompanyListWidget'
+import { EmployeeListWidget, } from '../../widgets/EmployeeListWidget'
 import styles from './CompaniesPage.module.css'
 
 export const CompaniesPage = (): JSX.Element => {
   return (
     <main className={styles.page}>
       <CompanyListWidget/>
+
+      <EmployeeListWidget/>
     </main>
   )
 }
